355f385ac1c1e815d4f0f769aca9c66090c1da95
quartzs2 <csh001231@gmail.com>
2025-04-29 18:05:07 +0900

    feat: api routes 작성(#1)
    - 전체 포켓몬 데이터를 반환하는 GET 요청 /api/pokemon
    - 개별 포켓몬 데이터를 반환하는 GET 요청 /api/pokemon/[pokemonId]

---
+++ app/api/pokemon/[pokemonId]/route.ts
@@ -0,0 +1,13 @@
+import { getPokeData } from "@api/pokeAPI";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }: { params: { pokemonId: string } }) {
+  const { pokemonId } = params;
+  const data = await getPokeData(Number(pokemonId));
+
+  if (data === null) {
+    return NextResponse.json({ error: "Interanl Server Error" }, { status: 500 });
+  }
+
+  return NextResponse.json({ pokemonData: data });
+}
